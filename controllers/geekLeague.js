581963331c385c38f13e32762c155d46b4f8957a
Yvan Menuet <yvan.menuet@gmail.com>
2020-09-29 19:19:14 -0500

    new routes for geekleague CRUD

--- controllers/geekLeague.js
+++ controllers/geekLeague.js
@@ -79,3 +79,85 @@ exports.getUserLeagues = async(req, res) => {
         geekLeagues: user.geekLeagues
     })
 }
+
+exports.editLeague = async(req, res) => {
+    const {
+        geekLeagueID
+    } = req.params
+    const {
+        name,
+        geeks
+    } = req.body
+    const editedGeekLeague = await GeekLeague.findOne({
+        _id: geekLeagueID
+    })
+    if (editedGeekLeague.creator.toString() !== req.user._id.toString()) return res.status(304).json({
+        message: {
+            en: 'You are not authorized to modify that item.',
+            fr: `Tu n'es pas autorisé à modifier cet élément.`
+        }
+    })
+    editedGeekLeague.name = name ? name : editedGeekLeague.name
+    editedGeekLeague.geeks = geeks ? [...editedGeekLeague.geeks, ...geeks] : editedGeekLeague.geeks
+    const users = await User.find({
+        _id: {
+            $in: geeks
+        }
+    })
+    await editedGeekLeague.save()
+    users.forEach(user => {
+        user.geekLeagues.push(geekLeagueID)
+        user.save()
+    })
+    const geekLeague = await GeekLeague.findById(geekLeagueID)
+        .populate({
+            path: 'geeks',
+            model: 'User'
+        })
+    res.status(200).json({
+        geekLeague
+    })
+}
+
+exports.deleteLeague = async(req, res) => {
+    const {
+        geekLeagueID
+    } = req.params
+    const geekLeague = await GeekLeague.findById(geekLeagueID)
+    if (geekLeague.creator.toString() !== req.user._id.toString()) return res.status(304).json({
+        message: {
+            en: 'You are not authorized to delete that item.',
+            fr: `Tu n'es pas autorisé à supprimer cet élément.`
+        }
+    })
+    await GeekLeague.findByIdAndDelete(geekLeagueID)
+    res.status(200).json({
+        message: {
+            en: 'Geek league deleted.',
+            fr: `Ligue Geek supprimée.`
+        }
+    })
+}
+
+exports.outLeague = async(req, res) => {
+    const {
+        geekLeagueID
+    } = req.params
+    const userID = req.user._id
+    const geekLeague = await GeekLeague.findOne({
+        _id: geekLeagueID
+    })
+    geekLeague.geeks = geekLeague.geeks.filter(geek => geek.toString() !== userID.toString())
+    await geekLeague.save()
+    const user = await User.findOne({
+        _id: userID
+    })
+    user.geekLeagues = user.geekLeagues.filter(league => league.toString() !== geekLeagueID.toString())
+    await user.save()
+    res.status(200).json({
+        message: {
+            en: 'Geek removed from geek league.',
+            fr: `Le geek a été retiré de la ligue.`
+        }
+    })
+}
--- routes/geekLeague.js
+++ routes/geekLeague.js
@@ -9,7 +9,10 @@ const {
 const {
     newLeagueProcess,
     getLeague,
-    getUserLeagues
+    getUserLeagues,
+    editLeague,
+    deleteLeague,
+    outLeague
 } = require('../controllers/geekLeague')
 
 router.post('/', ensureLogin, catchErrors(newLeagueProcess))
@@ -17,5 +20,11 @@ router.post('/', ensureLogin, catchErrors(newLeagueProcess))
 router.get('/:geekLeagueID', ensureLogin, catchErrors(getLeague))
 
 router.get('/', ensureLogin, catchErrors(getUserLeagues))
+
+router.get('/out/:geekLeagueID', ensureLogin, catchErrors(outLeague))
+
+router.put('/:geekLeagueID', ensureLogin, catchErrors(editLeague))
+
+router.delete('/:geekLeagueID', ensureLogin, catchErrors(deleteLeague))
 
 module.exports = router;
