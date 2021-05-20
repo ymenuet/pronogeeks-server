dd4b97fcecc038c8af1a2c36bbf9d5229f39625f
Yvan Menuet <yvan.menuet@gmail.com>
2021-05-19 20:49:58 -0500

    new build with email and username validation.

--- public/build/precache-manifest.69c4cbce0f9d8b08c398af230ebb04b7.js
+++ public/build/precache-manifest.69c4cbce0f9d8b08c398af230ebb04b7.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e79cf11ede1e5e04d62089e992c1a55d",
+    "revision": "4657b8c61eeef326af75b903ce4084b8",
     "url": "/index.html"
   },
   {
-    "revision": "6ee6523aa974a5a11e35",
+    "revision": "a8fe7a03f5660baf2af5",
     "url": "/static/css/2.8b2d98d4.chunk.css"
   },
   {
-    "revision": "68bc76eef299f2e515db",
+    "revision": "b0b2a0eac3ab4d0fae69",
     "url": "/static/css/main.d207b0f7.chunk.css"
   },
   {
-    "revision": "6ee6523aa974a5a11e35",
-    "url": "/static/js/2.50f1a148.chunk.js"
+    "revision": "a8fe7a03f5660baf2af5",
+    "url": "/static/js/2.a20ec234.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.50f1a148.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.a20ec234.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68bc76eef299f2e515db",
-    "url": "/static/js/main.c3c9f9d4.chunk.js"
+    "revision": "b0b2a0eac3ab4d0fae69",
+    "url": "/static/js/main.287990c7.chunk.js"
   },
   {
     "revision": "9b4e396c183e42c1fa5c",
--- public/build/service-worker.js
+++ public/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.002bcece7de7e9b04202b82540a8f0fe.js"
+  "/precache-manifest.69c4cbce0f9d8b08c398af230ebb04b7.js"
 );
 
 self.addEventListener('message', (event) => {
