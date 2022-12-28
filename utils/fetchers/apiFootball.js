90df193b1663d6635bb4c56089583224b5c2a596
Yvan Menuet <yvan.menuet@gmail.com>
2022-12-27 23:44:19 -0500

    Fix odds API issue.

--- utils/fetchers/apiFootball.js
+++ utils/fetchers/apiFootball.js
@@ -1,114 +1,126 @@
 const axios = require("axios");
 
 const apiFootballHeaders = {
-  "content-type": "application/octet-stream",
-  "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-  "x-rapidapi-key": process.env.APIFOOTBALL_KEY,
-  useQueryString: true,
+    "content-type": "application/octet-stream",
+    "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+    "x-rapidapi-key": process.env.APIFOOTBALL_KEY,
+    useQueryString: true,
 };
 
 const apiFootballV2BaseUrl = "https://api-football-v1.p.rapidapi.com/v2/";
 
-exports.getLeaguesByCountry = async (country) => {
-  const {
-    data: { api },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}leagues/country/${country}`,
-    headers: apiFootballHeaders,
-  });
+exports.getLeaguesByCountry = async(country) => {
+    const {
+        data: {
+            api
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}leagues/country/${country}`,
+        headers: apiFootballHeaders,
+    });
 
-  return api;
+    return api;
 };
 
-exports.getSeasonFromAPI = async (leagueID) => {
-  const {
-    data: {
-      api: {
-        leagues: [season],
-      },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}leagues/league/${leagueID}`,
-    headers: apiFootballHeaders,
-  });
+exports.getSeasonFromAPI = async(leagueID) => {
+    const {
+        data: {
+            api: {
+                leagues: [season],
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}leagues/league/${leagueID}`,
+        headers: apiFootballHeaders,
+    });
 
-  return season;
+    return season;
 };
 
-exports.getTeamsBySeasonFromAPI = async (leagueID) => {
-  const {
-    data: {
-      api: { teams },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}teams/league/${leagueID}`,
-    headers: apiFootballHeaders,
-  });
+exports.getTeamsBySeasonFromAPI = async(leagueID) => {
+    const {
+        data: {
+            api: {
+                teams
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}teams/league/${leagueID}`,
+        headers: apiFootballHeaders,
+    });
 
-  return teams;
+    return teams;
 };
 
-exports.getSeasonRankingFromAPI = async (leagueID) => {
-  const {
-    data: {
-      api: { standings },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}leagueTable/${leagueID}`,
-    headers: apiFootballHeaders,
-  });
+exports.getSeasonRankingFromAPI = async(leagueID) => {
+    const {
+        data: {
+            api: {
+                standings
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}leagueTable/${leagueID}`,
+        headers: apiFootballHeaders,
+    });
 
-  return standings;
+    return standings;
 };
 
-exports.getFixturesBySeasonFromAPI = async (leagueID) => {
-  const {
-    data: {
-      api: { fixtures },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}fixtures/league/${leagueID}`,
-    headers: apiFootballHeaders,
-    params: {
-      timezone: "Europe/London",
-    },
-  });
+exports.getFixturesBySeasonFromAPI = async(leagueID) => {
+    const {
+        data: {
+            api: {
+                fixtures
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}fixtures/league/${leagueID}`,
+        headers: apiFootballHeaders,
+        params: {
+            timezone: "Europe/London",
+        },
+    });
 
-  return fixtures;
+    return fixtures;
 };
 
-exports.getFixturesByMatchweekFromAPI = async (leagueID, matchweekNum) => {
-  const {
-    data: {
-      api: { fixtures },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}fixtures/league/${leagueID}/Regular_Season_-_${matchweekNum}`,
-    headers: apiFootballHeaders,
-    params: {
-      timezone: "Europe/London",
-    },
-  });
+exports.getFixturesByMatchweekFromAPI = async(leagueID, matchweekNum) => {
+    const {
+        data: {
+            api: {
+                fixtures
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}fixtures/league/${leagueID}/Regular_Season_-_${matchweekNum}`,
+        headers: apiFootballHeaders,
+        params: {
+            timezone: "Europe/London",
+        },
+    });
 
-  return fixtures;
+    return fixtures;
 };
 
-exports.getWinnerOddByFixtureFromAPI = async (fixtureID) => {
-  const {
-    data: {
-      api: { odds },
-    },
-  } = await axios({
-    method: "GET",
-    url: `${apiFootballV2BaseUrl}odds/fixture/${fixtureID}/label/1`,
-    headers: apiFootballHeaders,
-  });
+exports.getWinnerOddByFixtureFromAPI = async(fixtureID) => {
+    const {
+        data: {
+            api: {
+                odds
+            },
+        },
+    } = await axios({
+        method: "GET",
+        url: `${apiFootballV2BaseUrl}odds/fixture/${fixtureID}`,
+        headers: apiFootballHeaders,
+    });
 
-  return odds[0];
+    return odds[0];
 };
